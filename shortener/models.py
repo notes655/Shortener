afd93705151e76ec9cff10a95e6105a1b4cc2273
notes655 <notes655@gmail.com>
2022-08-09 20:41:14 +0900

    User Data Handling

---
+++ shortener/models.py
@@ -0,0 +1,19 @@
+from cgi import print_exception
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+# Create your models here.
+
+class PayPlan(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+class Users(AbstractUser):
+    pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
+
+class UserDetail(models.Model):
+    user = models.OneToOneField(Users,on_delete=models.CASCADE)
+    pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
