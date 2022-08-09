07db49b731b62e6a9ac8568649c05b60280f4ef2
notes655 <notes655@gmail.com>
2022-08-09 22:00:18 +0900

    model view url

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from shortener.models import Users
 # Create your views here.
+
+def index(request):
+    user = Users.objects.filter(username="admin").first()
+    email = user.email if user else "Anonymous User!"
+    print(email)
+    print(request.user.is_authenticated)
+    if request.user.is_authenticated is False:
+        email = "Anonymous User!"
+    print(email)
+    return render(request, "base.html",{"welcome_msg": "Hello Django"})
+
+def redirect_test(request):
+    print("Go Redirect")
+    return redirect("index")
